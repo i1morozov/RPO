69504fdbf9cd1c6609ae1442f2220d9996f65c15
ilya <i1morozov@yandex.ru>
2024-03-25 23:22:33 +0300

    lr2

--- app/src/main/java/com/example/lr_rpo1/MainActivity.java
+++ app/src/main/java/com/example/lr_rpo1/MainActivity.java
@@ -1,11 +1,24 @@
 package com.example.lr_rpo1;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lr_rpo1.databinding.ActivityMainBinding;
+
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +29,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
+
+    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        // обработка результата
+                        String pin = data.getStringExtra("pin");
+                        Toast.makeText(MainActivity.this, pin, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,17 +55,40 @@ public class MainActivity extends AppCompatActivity {
         byte[] shifrArr = encrypt(keys, v);
 
         byte[] deshArr = decrypt(keys, shifrArr);
-        TextView tv = binding.sampleText;
-        tv.setText(stringFromJNI());
+        //TextView tv = binding.sampleText;
+        //tv.setText(stringFromJNI());
+
     }
+
+    public static byte[] stringToHex(String s) {
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(s.toCharArray());
+        } catch (DecoderException ex) {
+            hex = null;
+        }
+        return hex;
+    }
+
+
+    public void onButtonClick(View v) {
+        Intent it = new Intent(this, PinpadActivity.class);
+        //startActivity(it);
+        mStartForResult.launch(it);
+    }
+
 
     /**
      * A native method that is implemented by the 'lr_rpo1' native library,
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
     public static native int initRng();
+
     public static native byte[] randomBytes(int no);
+
     public static native byte[] encrypt(byte[] key, byte[] data);
+
     public static native byte[] decrypt(byte[] key, byte[] data);
 }
